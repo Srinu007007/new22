f8ff9540737cb0e9f09713cc02a0329211e542a3
John <john222@gmail.com>
2024-08-01 22:46:18 +0530

    fask

---
+++ Awt1.java
@@ -0,0 +1,8 @@
+mport java.awt.*;
+class Awt2 extends Frame{
+Awt2(String s)
+super(s);
+setSize(200,200);
+setVisible(true);
+public static void main(Stringll args)
+Awt2 a = new Awt2("Welcome to awt");
